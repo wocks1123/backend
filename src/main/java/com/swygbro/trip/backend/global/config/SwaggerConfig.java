8611a4b09716eeca09c756eb00b10813963ab811
wocks1123 <122430128+DevRyu10@users.noreply.github.com>
2024-04-25 21:24:41 +0900

    feat: versioning 추가
    - main이나 dev에 병합 전 `gradle.properties`의 version 변경

--- src/main/java/com/swygbro/trip/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/swygbro/trip/backend/global/config/SwaggerConfig.java
@@ -3,11 +3,16 @@ package com.swygbro.trip.backend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${version}")
+    private String version;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI().components(new Components())
@@ -16,9 +21,9 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Trip API")
-                .description("Trip API")
-                .version("0.1.0");
+                .title("Matthew")
+                .description("Matthew에서 사용하는 API를 확인할 수 있습니다.")
+                .version(version);
     }
 
 }
