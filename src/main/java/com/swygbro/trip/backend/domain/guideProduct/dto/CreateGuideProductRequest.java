a9554883fd89d761141b3cc0236ac0fd7d031945
itavita08 <dudwns10000@gmail.com>
2024-04-25 20:23:07 +0900

    feat: 가이드 상품 APIswagger 추가
    - 이미지 등록, 가이드 상품 조회 swagger 추가

--- src/main/java/com/swygbro/trip/backend/domain/guideProduct/dto/CreateGuideProductRequest.java
+++ src/main/java/com/swygbro/trip/backend/domain/guideProduct/dto/CreateGuideProductRequest.java
@@ -1,12 +1,18 @@
 package com.swygbro.trip.backend.domain.guideProduct.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class CreateGuideProductRequest {
+    @NotBlank
+    @Schema(description = "상품 제목", example = "신나는 서울 투어")
     private String title;
+    @NotBlank
+    @Schema(description = "상품 설명", example = "서울 *** 여행 가이드 합니다.")
     private String description;
 
 }
--- src/main/java/com/swygbro/trip/backend/domain/guideProduct/dto/GuideProductResponse.java
+++ src/main/java/com/swygbro/trip/backend/domain/guideProduct/dto/GuideProductResponse.java
@@ -1,5 +1,6 @@
 package com.swygbro.trip.backend.domain.guideProduct.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,8 +9,11 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class GuideProductResponse {
+    @Schema(description = "상품 제목", example = "신나는 서울 투어")
     private String title;
+    @Schema(description = "상품 설명", example = "서울 *** 여행 가이드 합니다.")
     private String description;
+    @Schema(description = "상품 이미지")
     private List<GuideImageDto> images;
 
     public static GuideProductResponse fromDto(GuideProductDto dto) {
