bf08602ca9bfa9eef78b298fefc28c1bb846f65f
Mingyu <min1925k@gmail.com>
2024-04-29 15:59:42 +0900

    feat: change param id to merchantUid

--- src/main/java/com/swygbro/trip/backend/domain/reservation/api/ReservationController.java
+++ src/main/java/com/swygbro/trip/backend/domain/reservation/api/ReservationController.java
@@ -4,6 +4,7 @@ import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import com.swygbro.trip.backend.domain.reservation.aplication.ReservationService;
+import com.swygbro.trip.backend.domain.reservation.dto.ReservationDto;
 import com.swygbro.trip.backend.domain.reservation.dto.SavePaymentRequest;
 import com.swygbro.trip.backend.domain.reservation.dto.SaveReservationRequest;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,17 +18,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/reservation")
 @RequiredArgsConstructor
 @Slf4j
-@Tag(name = "Payments", description = "결제 API")
+@Tag(name = "Reservation", description = "결제 API")
 public class ReservationController {
     private final ReservationService reservationService;
 
-    @PostMapping("/validation/{imp_uid}")
-    @Operation(summary = "결제 검증", description = "아임포트 서버로부터 결제 정보를 검증합니다.")
+    @PostMapping("/client/save")
+    @Operation(summary = "예약 정보 저장", description = "예약 정보를 저장합니다.", tags = "Client")
+    @ApiResponse(
+            responseCode = "200",
+            description = "예약 정보 저장 성공",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = String.class)))
+    @ApiResponse(
+            responseCode = "500",
+            description = "예약 정보 저장 실패",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = String.class))
+    )
+    public ResponseEntity<String> saveReservation(@RequestBody SaveReservationRequest orderDto) {
+        try {
+            log.info("Received orders: {}", orderDto.toString());
+            return ResponseEntity.ok(reservationService.saveReservation(orderDto));
+        } catch (Exception e) {
+            log.info("Failed to receive orders: {}", orderDto.toString());
+            return ResponseEntity.badRequest().body("예약 정보 저장에 실패했습니다.");
+        }
+    }
+
+    @PostMapping("/client/{imp_uid}")
+    @Operation(summary = "결제 검증", description = "아임포트 서버로부터 결제 정보를 검증합니다.", tags = "Client")
     @ApiResponse(
             responseCode = "200",
             description = "결제 정보 검증 성공",
@@ -41,12 +68,10 @@ public class ReservationController {
                     mediaType = "application/json",
                     schema = @Schema(implementation = IamportResponse.class)))
     public IamportResponse<Payment> validateIamport(@PathVariable String imp_uid) throws IamportResponseException, IOException {
-        log.info("imp_uid: {}", imp_uid);
-        log.info("validateIamport");
         return reservationService.validateIamport(imp_uid);
     }
 
-    @PostMapping("/payment/save")
+    @PostMapping("/client/payment")
     @Operation(summary = "결제 정보 저장", description = "결제 정보를 저장합니다.")
     @ApiResponse(
             responseCode = "200",
@@ -71,32 +96,8 @@ public class ReservationController {
         }
     }
 
-    @PostMapping("/save")
-    @Operation(summary = "예약 정보 저장", description = "예약 정보를 저장합니다.")
-    @ApiResponse(
-            responseCode = "200",
-            description = "예약 정보 저장 성공",
-            content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = String.class)))
-    @ApiResponse(
-            responseCode = "500",
-            description = "예약 정보 저장 실패",
-            content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = String.class))
-    )
-    public ResponseEntity<String> saveReservation(@RequestBody SaveReservationRequest orderDto) {
-        try {
-            log.info("Received orders: {}", orderDto.toString());
-            return ResponseEntity.ok(reservationService.saveReservation(orderDto));
-        } catch (Exception e) {
-            log.info("Failed to receive orders: {}", orderDto.toString());
-            return ResponseEntity.badRequest().body("예약 정보 저장에 실패했습니다.");
-        }
-    }
 
-    @PostMapping("/cancel/{reservation_id}")
+    @PostMapping("/client/cancel/{merchant_uid}")
     @Operation(summary = "예약 취소", description = "예약 및 결제 취소 요청합니다.")
     @ApiResponse(
             responseCode = "200",
@@ -104,8 +105,18 @@ public class ReservationController {
             content = @Content(
                     mediaType = "application/json",
                     schema = @Schema(implementation = String.class)))
-    public ResponseEntity<String> cancelPayment(@PathVariable String reservation_id) {
-        return ResponseEntity.ok(reservationService.cancelReservation(reservation_id));
+    public ResponseEntity<String> cancelPayment(@PathVariable String merchant_uid) {
+        return ResponseEntity.ok(reservationService.cancelReservation(merchant_uid));
+    }
+
+    @GetMapping("client/list")
+    public ResponseEntity<List<ReservationDto>> getReservationList() {
+        return ResponseEntity.ok(reservationService.getReservationList());
+    }
+
+    @GetMapping("client/{merchant_uid}")
+    public ResponseEntity<ReservationDto> getReservation(@PathVariable String merchant_uid) {
+        return ResponseEntity.ok(reservationService.getReservation(merchant_uid));
     }
 }
 
