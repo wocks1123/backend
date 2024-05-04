835a01c3d46412409f249dfd05828109a7c5f56d
wocks1123 <54695103+wocks1123@users.noreply.github.com>
2024-05-04 14:48:16 +0900

    feat: @CurrentUser 추가
    - jwt에서 추출한 유저 정보를 추출하는 기능

---
+++ src/main/java/com/swygbro/trip/backend/global/jwt/CurrentUser.java
@@ -0,0 +1,14 @@
+package com.swygbro.trip.backend.global.jwt;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "user")
+public @interface CurrentUser {
+}
