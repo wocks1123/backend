37f7792b50d2fe617d7eaee1b9612c8d04b2b0ad
wocks1123 <wocks1123@gmail.com>
2024-05-08 23:58:38 +0900

    feat: 디스코드봇 추가
    - 서버 시작 시 디스코드 메시지 전송
    - OpenFeignClient 추가
    - application.properties에 관련 설정 추가

---
+++ src/main/java/com/swygbro/trip/backend/infra/discordbot/DiscordMessage.java
@@ -0,0 +1,11 @@
+package com.swygbro.trip.backend.infra.discordbot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class DiscordMessage {
+    private String content;
+}
