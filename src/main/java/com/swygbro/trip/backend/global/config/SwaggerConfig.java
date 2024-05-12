794b54e274d0e95ccff7a95134ab13e3a3dee11c
Mingyu <min1925k@gmail.com>
2024-05-13 01:15:51 +0900

    chore: Swagger Tag description 간략화

--- src/main/java/com/swygbro/trip/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/swygbro/trip/backend/global/config/SwaggerConfig.java
@@ -3,8 +3,8 @@ package com.swygbro.trip.backend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.tags.Tag;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,35 +40,8 @@ public class SwaggerConfig {
 
     private List<Tag> tagList() {
         return List.of(
-                new Tag().name("Reservation-Client").description("""
-                                                
-                        ### 개요
-                                
-                        - 여행객의 예약 관련 기능을 제공합니다.
-                                
-                        ### 주요 기능
-                                
-                        - 예약 정보 조회
-                        - 예약 리스트 조회
-                        - 결제 검증
-                        - 예약 정보 저장
-                        - 결제 정보 저장
-                        - 예약 취소
-
-                        """),
-                new Tag().name("Reservation-Guide").description("""
-                                                
-                        ### 개요
-                                
-                        - 가이드의 예약 관련 기능을 제공합니다.
-                                
-                        ### 주요 기능
-                                
-                        - 예약 리스트 조회
-                        - 예약 확정
-                        - 예약 취소
-
-                        """)
+                new Tag().name("Reservation-Client").description("여행객의 예약 관련 기능을 제공합니다."),
+                new Tag().name("Reservation-Guide").description("가이드의 예약 관련 기능을 제공합니다.")
         );
     }
 }
