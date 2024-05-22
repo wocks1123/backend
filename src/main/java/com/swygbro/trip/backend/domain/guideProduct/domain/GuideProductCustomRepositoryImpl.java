77a76eea5ed080b725497458e0377eb8b095d31b
itavita08 <dudwns10000@gmail.com>
2024-05-23 02:17:18 +0900

    refactor: 시간대 수정
    - zonedDateTime에서 LocalDate로 변환할 경우 타임존이 적용 안되서 UTC 날짜 그대로 변환되는 상황 발생
    - 임의로 +9시간을 해줘서 한국 시간대로 변환

--- src/main/java/com/swygbro/trip/backend/domain/guideProduct/domain/GuideProductCustomRepositoryImpl.java
+++ src/main/java/com/swygbro/trip/backend/domain/guideProduct/domain/GuideProductCustomRepositoryImpl.java
@@ -53,8 +53,8 @@ public class GuideProductCustomRepositoryImpl implements GuideProductCustomRepos
                 .select(new QSearchGuideProductResponse(
                         qProduct.id,
                         qProduct.title,
-                        qProduct.locationName,
                         qProduct.thumb,
+                        qProduct.locationName,
                         qProduct.guideStart,
                         qProduct.guideEnd
                 ))
@@ -70,8 +70,8 @@ public class GuideProductCustomRepositoryImpl implements GuideProductCustomRepos
                 .select(new QSearchGuideProductResponse(
                         qProduct.id,
                         qProduct.title,
-                        qProduct.locationName,
                         qProduct.thumb,
+                        qProduct.locationName,
                         qProduct.guideStart,
                         qProduct.guideEnd
                 ))
@@ -87,8 +87,8 @@ public class GuideProductCustomRepositoryImpl implements GuideProductCustomRepos
                 .select(new QSearchGuideProductResponse(
                         qProduct.id,
                         qProduct.title,
-                        qProduct.locationName,
                         qProduct.thumb,
+                        qProduct.locationName,
                         qProduct.guideStart,
                         qProduct.guideEnd
                 ))
@@ -120,8 +120,8 @@ public class GuideProductCustomRepositoryImpl implements GuideProductCustomRepos
                 .select(new QSearchGuideProductResponse(
                         qProduct.id,
                         qProduct.title,
-                        qProduct.locationName,
                         qProduct.thumb,
+                        qProduct.locationName,
                         qProduct.guideStart,
                         qProduct.guideEnd
                 ))
